efb8ab5558874460d3f5e458f18ed9bf513eec49
Israel Berko <berkois@gmail.com>
2022-01-31 02:12:34 +0200

    feat: renovate the welcome page

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,32 @@
 const welcomeWindow = document.querySelector(".welcome");
 const welcomeMessage = welcomeWindow.querySelector(".welcome__message");
 const welcomeForm = welcomeWindow.querySelector(".welcome__form_type_welcome");
-const activityForm = welcomeWindow.querySelector(".welcome__form_type_activity");
-const formVisibilityClass = "welcome__form_visible";
+const formInvisibilityClass = "welcome__form_invisible";
 const nameInput = welcomeForm.querySelector(".welcome__input");
-const initialName = "ברקואי יקר";
+const welcomeGoButton = welcomeWindow.querySelector(".welcome__go-button");
+
+const showGoButton = () => {
+  welcomeGoButton.classList.add("welcome__go-button_active");
+  welcomeGoButton.style.animationPlayState = "running";
+};
 
 const showWelcomeMessage = (givenName) => {
   welcomeMessage.textContent = `ברוך הבא, ${givenName}!`;
-};
-const makeFormVisible = (form) => {
-  form.classList.add(formVisibilityClass);
-  welcomeMessage.classList.add("welcome__message_top");
+  welcomeMessage.classList.add("welcome__message_active");
 };
 
 const makeFormInvisible = (form) => {
-  form.classList.remove(formVisibilityClass);
-  welcomeMessage.classList.remove("welcome__message_top");
+  form.classList.add(formInvisibilityClass);
 };
 
-welcomeWindow.addEventListener("click", () => {
-  if (!(welcomeForm.classList.contains(formVisibilityClass) || activityForm.classList.contains(formVisibilityClass))) {
-    makeFormVisible(welcomeForm);
-  }
+document.addEventListener("click", () => {
+  welcomeWindow.classList.add("welcome_no_animation");
 });
 
 welcomeForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
   const userName = nameInput.value;
   makeFormInvisible(welcomeForm);
-  makeFormVisible(activityForm);
   showWelcomeMessage(userName);
+  showGoButton();
 });
-
-showWelcomeMessage(initialName);
